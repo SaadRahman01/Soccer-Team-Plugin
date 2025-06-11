28e84f7e80aff4eaba60dfda9c0d7580d0cbb8e0
SaadRahman01 <saad.rahman010@gmail.com>
2025-06-11 12:20:33 +0500

    Added version file and DB structure for soccer manager plugin

---
+++ local/soccermanager/version.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin version and other meta-data are defined here.
+ *
+ * @package    local_soccermanager
+ * @copyright  2025 Saad Rahman
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_soccermanager';
+$plugin->version = 2025061100;
+$plugin->requires = 2022112800;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
