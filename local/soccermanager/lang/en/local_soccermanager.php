cf9d29297aec7f334a85d6ed0419a518c0f89c18
SaadRahman01 <saad.rahman010@gmail.com>
2025-06-11 13:54:24 +0500

    Added lang file for soccer manager plugin

---
+++ local/soccermanager/lang/en/local_soccermanager.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * English language strings for the soccer manager plugin
+ *
+ * @package    local_soccermanager
+ * @copyright  2025 Saad Rahman
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Soccer Team Manager';
+$string['soccermanager'] = 'Soccer Team';
+$string['soccermanager:manage'] = 'Manage soccer team assignments';
+$string['soccermanager:view'] = 'View soccer team assignments';
+
+// Form strings
+$string['assignstudent'] = 'Assign Student';
+$string['selectstudent'] = 'Select Student';
+$string['selectposition'] = 'Select Position';
+$string['jerseynumber'] = 'Jersey Number';
+$string['save'] = 'Save Assignment';
+$string['cancel'] = 'Cancel';
+$string['currentassignments'] = 'Current Soccer Team Assignments';
+
+// Position strings
+$string['position_goalkeeper'] = 'Goalkeeper';
+$string['position_defender'] = 'Defender';
+$string['position_midfielder'] = 'Midfielder';
+$string['position_forward'] = 'Forward';
+
+// Messages
+$string['assignmentsaved'] = 'Soccer team assignment saved successfully!';
+$string['assignmentdeleted'] = 'Soccer team assignment deleted successfully!';
+$string['jerseynumberinuse'] = 'Jersey number {$a} is already assigned to another student in this course.';
+$string['studentalreadyassigned'] = 'This student already has a soccer team assignment in this course.';
+$string['noassignments'] = 'No soccer team assignments found for this course.';
+
+// Table headers
+$string['student'] = 'Student';
+$string['position'] = 'Position';
+$string['jersey'] = 'Jersey #';
+$string['actions'] = 'Actions';
+$string['edit'] = 'Edit';
+$string['delete'] = 'Delete';
