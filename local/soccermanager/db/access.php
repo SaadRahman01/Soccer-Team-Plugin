9ce748f558c647746d7bee4772224305f0f036cb
SaadRahman01 <saad.rahman010@gmail.com>
2025-06-11 12:41:02 +0500

    Added capabilities for soccer manager plugin

---
+++ local/soccermanager/db/access.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Capability definitions for the soccer manager plugin
+ *
+ * @package    local_soccermanager
+ * @copyright  2025 Saad Rahman
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'local/soccermanager:manage' => [
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ],
+    'local/soccermanager:view' => [
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ]
+];
